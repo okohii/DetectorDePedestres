da602c9b589c679f56604685465c2e96d531b232
Gustavo Gil <gustavo.gil@sptech.school>
2025-02-15 02:49:31 -0300

    melhoria em tempo real

--- Detector.py
+++ Detector.py
@@ -1,24 +1,29 @@
-import cv2 
-import imutils 
-   
-hog = cv2.HOGDescriptor() 
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector()) 
-   
-image = cv2.imread('img.png') 
-   
-image = imutils.resize(image, 
-                       width=min(400, image.shape[1])) 
-   
-(regions, _) = hog.detectMultiScale(image,  
-                                    winStride=(4, 4), 
-                                    padding=(4, 4), 
-                                    scale=1.05) 
-   
-for (x, y, w, h) in regions: 
-    cv2.rectangle(image, (x, y),  
-                  (x + w, y + h),  
-                  (0, 0, 255), 2) 
-cv2.imshow("Image", image) 
-cv2.waitKey(0) 
-   
-cv2.destroyAllWindows() 
+from ultralytics import YOLO
+import cv2
+
+print(f'OpenCV Version: {cv2.__version__}')
+
+modelo = YOLO('yolov8n.pt')
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Erro ao abrir a câmera!")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Falha ao capturar imagem")
+        break
+
+    resultados = modelo(frame)
+
+    modelo.predict(source='0', show=True);
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()

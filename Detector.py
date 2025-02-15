8dbd659fa9c28ed0197b10daa91c5dd1ce64cc4c
Gustavo Gil <gustavo.gil@sptech.school>
2025-02-15 03:10:59 -0300

    teste com varios modelos e melhorias de precisão

--- Detector.py
+++ Detector.py
@@ -1,15 +1,25 @@
 from ultralytics import YOLO
 import cv2
+import numpy as np
 
 print(f'OpenCV Version: {cv2.__version__}')
 
-modelo = YOLO('yolov8n.pt')
+
+modelo = YOLO('yolov8l.pt')
 
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     print("Erro ao abrir a câmera!")
     exit()
+
+
+threshold = 0.5 
+
+cores_classes = {}
+
+def gerar_cor_unica():
+    return tuple(np.random.randint(0, 256, 3).tolist())
 
 while True:
     ret, frame = cap.read()
@@ -20,7 +30,23 @@ while True:
 
     resultados = modelo(frame)
 
-    modelo.predict(source='0', show=True);
+    detecções_filtradas = [det for det in resultados[0].boxes if det.conf > threshold]
+
+    for det in detecções_filtradas:
+        x1, y1, x2, y2 = det.xyxy[0].cpu().numpy()
+        x1, y1, x2, y2 = map(int, [x1, y1, x2, y2])
+        
+        label = modelo.names[int(det.cls)]
+
+        if label not in cores_classes:
+            cores_classes[label] = gerar_cor_unica()
+
+        cor = cores_classes[label]
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), cor, 2)
+        cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, cor, 2)
+
+    cv2.imshow("Detecção em Tempo Real", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
